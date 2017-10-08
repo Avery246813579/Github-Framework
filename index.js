9d0e822a5b5a10e6176f1eeb7fb8a13f19e7dfec
Avery Durrant <Avery246813579@gmail.com>
2017-10-08 00:32:50 -0400

    Added getting URL and reading the files

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 console.log("Test");
+
+let split = window.location.toString().split('/');
+let mySplit = [];
+let shouldAdd = false;
+
+for(let i = 0; i < split.length; i++){
+    if(split[i] !== ''){
+        if(split[i] === "github.com"){
+            shouldAdd = true;
+            continue;
+        }
+
+        if(shouldAdd){
+            if(mySplit.length < 2){
+                mySplit.push(split[i]);
+            }
+        }
+    }
+}
+
+if(mySplit.length === 2){
+    fetch('https://api.github.com/repos/' + mySplit[0] + '/' + mySplit[1] + '/contents/', {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        }
+    }).then((response) => response.json())
+        .then((responseJson) => {
+            console.dir(responseJson)
+        })
+        .catch((error) => {
+            alert("Internal Error" + JSON.stringify(error));
+        });
+}
+
+console.dir(mySplit);
